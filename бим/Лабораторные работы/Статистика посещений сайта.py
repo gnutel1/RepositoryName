151b54600e75ae9121519aa0c9662eeaf14cda80
gnutel1 <149432295+gnutel1@users.noreply.github.com>
2023-10-30 23:14:36 +0300

    Add files via upload

---
+++ бим/Лабораторные работы/Статистика посещений сайта.py
@@ -0,0 +1,6 @@
+users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
+kk = len(set(users))
+ll = len(users)
+slovar = {"Общее количество" : ll,
+          "Уникальные посещения" : kk}
+print(slovar)
---
+++ бим/Простые типы данных/Арифметические операторы и избегание магических чисел.py
@@ -0,0 +1,6 @@
+
+
+length = 90
+width = 50
+perimeter = 2 * width + 2 * length
+print("Периметр футбольного поля равен", perimeter)
