b254108d03d3da6dece55b37963d184845afbf94
gnutel1 <149432295+gnutel1@users.noreply.github.com>
2023-10-30 23:24:19 +0300

    Лабараторная работа 1

---
+++ Лабараторная работа 1/Составные типы данных/Встроенные функции sum, len, min, max, round.py
@@ -0,0 +1,7 @@
+list_digit = [4, 9, 5, 1, 8, 4]  # TODO сформировать список цифр
+print(list_digit)
+print("Сумма цифр", sum(list_digit))
+print("Количество цифр", len(list_digit))
+print("Минимальная цифра", min(list_digit))
+print("Максимальная цифра", max(list_digit))
+print("Среднее арифметическое цифр", round(sum(list_digit) / len(list_digit),2))
